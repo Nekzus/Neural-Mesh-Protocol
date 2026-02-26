42bd93075bace0f76e42c2889d42403b45af1873
Nekzus <85507589+Nekzus@users.noreply.github.com>
2026-02-25 22:20:43 -0300

    chore(sdk): remove node_modules and add .gitignore

--- sdk/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Neural Mesh Protocol Official SDK
- * @version 1.0.0-alpha
- *
- * Exporting the drop-in replacement classes for Model Context Protocol APIs.
- */
-export { NmpServer } from "./server.js";
-export { NmpMcpBridge } from "./bridge.js";
-export { z } from "zod";
